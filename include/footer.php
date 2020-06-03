3c51276e6bc39f52c1a916696cf15d9caaa23b72
Pedro L <42793973+akadarin@users.noreply.github.com>
2020-06-02 23:33:48 -0300

    Starting

---
+++ include/footer.php
@@ -0,0 +1,59 @@
+<footer>
+    <div class="row m-auto w-100 bg-purple txt-white pt-5 p-3">
+        <div class="col-md-2">
+
+        </div>
+
+        <div class="col-md-2">
+            <img src="images/logo-empresa/logo.png" class="img-fluid w-100" alt="">
+        </div>
+
+        <div class="col-md-3 lt-white m-auto">
+            <p class="small-lt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam praesentium perferendis
+                laboriosam reiciendis adipisci dignissimos est explicabo soluta modi fugit! Suscipit deserunt, beatae
+                voluptatibus obcaecati nostrum nemo et itaque ratione!</p>
+        </div>
+
+        <div class="col-md-3 lt-white m-auto">
+            <h4>Contate-nos</h4>
+            <p class="smaller-lt"><i class="fas fa-map-marker-alt"></i> Rua Aquidaban, 666 - Rolandia-PR - CEP 86666-030
+            </p>
+            <p class="smaller-lt"><i class="fas fa-phone-alt"></i> +55 43 7070-6070 | <i class="fab fa-whatsapp bg"></i>
+                +55 43 9 9999-9999?</p>
+            <p class="smaller-lt"><i class="fas fa-envelope"></i> contato@petriproject.com.br</p>
+        </div>
+
+        <div class="col-md-2">
+
+        </div>
+
+        <div class="col-md-2">
+
+        </div>
+
+        <div class="col-md-8">
+            <hr>
+        </div>
+
+        <div class="col-md-2">
+
+        </div>
+
+        <div class="col-md-2">
+
+        </div>
+
+        <div class="col-md-6">
+            <p class="pl-4">&copy; Copyright 2020 - Petri Network. Todos os direitos reservados.</p>
+        </div>
+
+        <div class="col-md-2 d-flex justify-content-end align-items-center">
+            <a href="" class="mr-2 px-2"><i class="fab fa-facebook-f lt-white"></i></a>
+            <a href="" class="px-2 mr-5"><i class="fab fa-instagram lt-white"></i></a>
+        </div>
+
+        <div class="col-md-2">
+
+        </div>
+    </div>
+</footer>
